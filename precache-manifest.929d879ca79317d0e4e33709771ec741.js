797abae0e0084a141c6c4e98d669f13df1af57d5
unknown <syahril.firdaus@dwidasa.com>
2022-06-17 17:07:46 +0700

    Updates

---
+++ precache-manifest.929d879ca79317d0e4e33709771ec741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c451cd4f77a991c61ac7215087b7b5",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "391299df6c0173ebd390",
+    "url": "/react-todo-app/static/css/main.4e987138.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/react-todo-app/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-todo-app/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391299df6c0173ebd390",
+    "url": "/react-todo-app/static/js/main.e4ee52ac.chunk.js"
+  },
+  {
+    "revision": "dc32543b2506e00c5ce7",
+    "url": "/react-todo-app/static/js/runtime-main.c4a6f585.js"
+  }
+]);
